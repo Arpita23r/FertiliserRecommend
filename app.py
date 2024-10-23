c8c5d64ee1c7c7ac7fb4588ed787a95a0fa24d82
Arpita Roy <100989922+Arpita23r@users.noreply.github.com>
2024-10-23 20:24:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-curl -X POST http://127.0.0.1:5000/suggest-fertilizer -H "Content-Type: application/json" -d "{\"Temparature\": 21, \"Humidity\": 34, \"Moisture\": 41, \"Soil_Type\": 5, \"Crop_Type\": 17, \"Nitrogen\": 47, \"Potassium\": 31, \"Phosphorous\": 36}"
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pickle
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+model = pickle.load(open('classifier.pkl', 'rb'))
+ferti_encoder = pickle.load(open('fertilizer.pkl', 'rb'))
+
+@app.route('/suggest-fertilizer', methods=['POST'])
+def suggest_fertilizer():
+    data = request.get_json()
+    
+    input_features = [
+        data['Temparature'],
+        data['Humidity'],
+        data['Moisture'],
+        data['Soil_Type'],
+        data['Crop_Type'],
+        data['Nitrogen'],
+        data['Potassium'],
+        data['Phosphorous']
+    ]
+    
+    prediction = model.predict([input_features])[0]
+    fertilizer_name = ferti_encoder.classes_[prediction]
+    
+    return jsonify({"Fertilizer": fertilizer_name})
+
+if __name__ == '__main__':
+    app.run(debug=True)
